e0a0025c891ac34c781913af5abddb9c6afb279d
jiangxufeng <jiangxufeng1998@qq.com>
2017-05-26 22:04:45 +0800

    对首次登录时用户的身份识别进行了完善

--- learn_api/account/urls.py
+++ learn_api/account/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url,include
 from .views import (UserLoginView, UserChangeNameView,
-                     UserDetailView, StuUserCreateView, ProUserCreateView,YzmView)
+                     UserDetailView, UserCreateView, YzmView)
 
 
 
@@ -8,8 +8,8 @@ urlpatterns = [
     url(r'^detail/$', UserDetailView.as_view(), name='user_detail'),
     url(r'^login/$', UserLoginView.as_view(), name='login'),
     #url(r'^logout/$', Us)
-    url(r'^stu_reg/$', StuUserCreateView.as_view(), name='stu_reg'),
-    url(r'^Pro_reg/$', ProUserCreateView.as_view(), name='pro_reg'),
+    url(r'^reg/$', UserCreateView.as_view(), name='reg'),
+   # url(r'^Pro_reg/$', ProUserCreateView.as_view(), name='pro_reg'),
     url(r'^yzm/', YzmView.as_view(), name='yzm'),
     url(r'^changename/$', UserChangeNameView.as_view(), name='change_name')
     #url(r'^sign/', SignInPublishView.as_view(), name='sign'),
