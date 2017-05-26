528cc9761145efd330b789dc9b5e09fd3141b6cd
gongmm <gnaizgnaw@163.com>
2017-05-26 13:06:25 +0800

    Revert "Revert "Revert "Add files via upload"""
    This reverts commit 2edbfd7fdfd5ce025631405021022fee1e2f3d75.

--- learn_api/post_q/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Comment, Question, Course, Post
-# Register your models here.
-
-admin.site.register(Post)
-admin.site.register(Course)
-admin.site.register(Comment)
-admin.site.register(Question)
